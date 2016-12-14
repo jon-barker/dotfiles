7f7b7795785d4f08952bc677a93a92e46506539a
Jon Barker <jbarker@nvidia.com>
2016-12-14 09:52:13 -0700

    Updated setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,7 @@
-cp vimrc ~/.vimrc
-
 rm -rf ~/.vim/bundle/Vundle.vim/
+
+cp vimrc ~/.vimrc
+cp tmux.conf ~/.tmux.conf
+
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
