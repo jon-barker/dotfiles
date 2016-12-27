db8329e6cd641e47f07258db032d18b3f755224c
Jon Barker <jbarker@nvidia.com>
2016-12-27 10:48:13 -0700

    Removed bashrc

--- setup.sh
+++ setup.sh
@@ -12,8 +12,9 @@ cp vimrc ~/.vimrc
 cp tmux.conf ~/.tmux.conf
 cp dircolors ~/.dircolors
 eval `dircolors ~/.dircolors`
-cp bashrc ~/.bashrc
 
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
 
+export TERM="screen-256color"
+alias tmux="tmux -2"
