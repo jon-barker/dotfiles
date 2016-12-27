9f191485c5980c2383382f1a304b4b820f7f3dad
Jon Barker <jbarker@nvidia.com>
2016-12-27 10:44:25 -0700

    Fixed tmux vim colors

--- setup.sh
+++ setup.sh
@@ -3,7 +3,6 @@ unzip solarized.zip
 mkdir ~/.vim/colors
 mv solarized/vim-colors-solarized/colors/solarized.vim ~/.vim/colors
 curl https://raw.github.com/seebi/dircolors-solarized/master/dircolors.256dark > ~/.dircolors
-source ~/.bashrc
 rm -r solarized
 rm solarized.zip
 
@@ -13,8 +12,8 @@ cp vimrc ~/.vimrc
 cp tmux.conf ~/.tmux.conf
 cp dircolors ~/.dircolors
 eval `dircolors ~/.dircolors`
+cp bashrc ~/.bashrc
 
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
 
-source ~/.bashrc
